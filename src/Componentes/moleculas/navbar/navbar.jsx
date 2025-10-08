2af2fbda1f9d134a019d24079fb4991c321f0aa3
ClaudioBarlassina <SoyHenryClaudio>
2025-10-08 12:58:33 -0300

    carrito

--- src/Componentes/moleculas/navbar/navbar.jsx
+++ src/Componentes/moleculas/navbar/navbar.jsx
@@ -26,12 +26,10 @@ const Navbar = () => {
   const OpenMenu = useStore((state) => state.botonMenu)
   const CloseMenu = useStore((state) => state.closeMenu)
   const Estado = useStore((state) => state.Menu)
-//  Estados Carrito
-  const EstadoOpenCarrito = useStore((state)=> state.menuCarrito)
-  const OpenMenuCarrito = useStore((state)=> state.OpenMenuCart)
-  const CloseMenuCarrito = useStore((state)=> state.ClosemenuCarrito)
-
-
+  //  Estados Carrito
+  const EstadoOpenCarrito = useStore((state) => state.menuCarrito)
+  const OpenMenuCarrito = useStore((state) => state.OpenMenuCart)
+  const CloseMenuCarrito = useStore((state) => state.ClosemenuCarrito)
 
   // lista productos
 
@@ -51,7 +49,6 @@ const Navbar = () => {
       imagen: imagenejemplo,
     },
   ])
-
 
   const aumentar = (id) => {
     setCarrito((prev) =>
@@ -119,12 +116,21 @@ const Navbar = () => {
 
       <h1 className="marca-titulo">Star</h1>
       <div className="iconos-sec">
-        <img className="icono-cart"src={cart} alt="" onClick={OpenMenuCarrito} />  
-        
+        <img
+          className="icono-cart"
+          src={cart}
+          alt=""
+          onClick={OpenMenuCarrito}
+        />
+
         <img className="icono-usuario" src={user} alt="" />
       </div>
       {/* Carrito */}
-      <div className={`carrito-contenedorGeneral ${EstadoOpenCarrito ? "open": ""}`}>
+      <div
+        className={`carrito-contenedorGeneral ${
+          EstadoOpenCarrito ? 'open' : ''
+        }`}
+      >
         {/* "carrito-contenedorGeneral" */}
         <div className="carrito-encabezado">
           <h3>Carrito</h3>
@@ -133,17 +139,19 @@ const Navbar = () => {
         <div>
           <Table striped bordered hover responsive>
             <thead>
-              <tr>
-               
-              
-              </tr>
+              <tr></tr>
             </thead>
 
             <tbody className="carrito-table-body">
               {carrito.map((p) => (
                 <tr key={p.id}>
-                  <td className='conteiner-imagen'>
-                    <img src={p.imagen} alt={p.nombre} width={100} className='carrito-table-body-img' />
+                  <td className="conteiner-imagen">
+                    <img
+                      src={p.imagen}
+                      alt={p.nombre}
+                      width={100}
+                      className="carrito-table-body-img"
+                    />
                   </td>
 
                   <td>
@@ -176,31 +184,34 @@ const Navbar = () => {
                   </td>
                 </tr>
               ))}
-              
+
               {carrito.length === 0 && (
                 <tr>
-                  <td colSpan={3} style={{ textAlign: 'center', fontFamily:"sans-serif" }}>
-                    El carrito está vacío 
+                  <td
+                    colSpan={3}
+                    style={{ textAlign: 'center', fontFamily: 'sans-serif' }}
+                  >
+                    El carrito está vacío
                   </td>
                 </tr>
               )}
             </tbody>
           </Table>
-        </div><div className="carrito-total-subtotal-finalizar">
-               <div className='carrito-subtotal'>
-                <p>Subtotal</p>
-                <p>$$12345</p>
-               </div>
-               <div className='carrito-total'>
-                <p>Total</p>
-                <p>$$123456</p>
-               </div>
-               <div className='carrito-buttons'>
-                <button className='finalizar'>FINALIZAR COMPRA</button>
-                <button className='seguirCom'>SEGUIR COMPRANDO</button>
-               </div>
-              </div>
-
+        </div>
+        <div className="carrito-total-subtotal-finalizar">
+          <div className="carrito-subtotal">
+            <p>Subtotal</p>
+            <p>$$12345</p>
+          </div>
+          <div className="carrito-total">
+            <p>Total</p>
+            <p>$$123456</p>
+          </div>
+          <div className="carrito-buttons">
+            <button className="finalizar">FINALIZAR COMPRA</button>
+            <button className="seguirCom">SEGUIR COMPRANDO</button>
+          </div>
+        </div>
       </div>
     </div>
   )
